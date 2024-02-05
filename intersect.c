88e03a671de51102a6e8961eb29b8179bc768866
Lwen1243 <1198807618@qq.com>
2024-02-05 21:09:05 +0800

    next function(char)

--- intersect.c
+++ intersect.c
@@ -3,7 +3,7 @@
 #define mod1 (long long)1000000007
 #define mod2 (long long)998244353
 
-void intersect_Row(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2)
+void intersect_Row_int(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2)
 {
     long long mp1[MAXROW] ;
     long long mp2[MAXROW] ;
@@ -52,27 +52,15 @@ void intersect_Row(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXROW],
     
 }
 
+void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2)
+{
+    
+}
+
+
+
 int main()
 {
-    int A[MAXROW][MAXCOL] ;
-    int B[MAXROW][MAXCOL] ;
-    for(int i = 0; i < MAXROW; i++){
-        for(int j = 0; j < MAXCOL; j++){
-            A[i][j] = -1;
-            B[i][j] = -1;
-        }
-    }
-    A[0][0] = 2 , A[0][1] = 2 , A[0][2] = 2 ;
-    A[1][0] = 0, A[1][1] = 0, A[1][2] = 1 ;
-    A[2][0] = 1, A[2][1] = 2, A[2][2] = 3;
-    A[3][0] = 1, A[3][1] = 1, A[3][2] = 1 ;
-    B[0][0] = 1, B[0][1] = 2, B[0][2] = 3;
-    B[1][0] = 2, B[1][1] = 2, B[1][2] = 2 ;
-    B[2][0] = 2, B[2][1] = 2, B[2][2] = 0;
     
-    int ii[11], jj[11] ;
-    int cnt1 = 0, cnt2 = 0;
-    intersect_Row(A, B, ii, jj, &cnt1, &cnt2) ;
-    for(int i = 1 ; i <= cnt1; i ++ ) printf("%d\n", ii[i]) ;
     return 0 ;
 }
--- intersect.h
+++ intersect.h
@@ -7,6 +7,7 @@
 #define MAXROW 105 
 #define MAXCOL 105
 
-void intersect_Row(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2) ;
+void intersect_Row_int(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2) ;
+void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2) ;
 
 #endif // intersect.h
