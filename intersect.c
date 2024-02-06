6b30509bfb4eca592c0481e1043f031f578e0127
Lwen1243 <1198807618@qq.com>
2024-02-07 00:53:46 +0800

    intersect(char)finish

--- intersect.c
+++ intersect.c
@@ -53,7 +53,7 @@ void intersect_Row_int(int a[MAXROW][MAXCOL], int b[MAXROW][MAXCOL], int ii[MAXR
 }
 
 /*
-intersect_Row_char我的想法是将整个char** 转为int**，之后使用intersect_Row_int
+intersect_Row_char我的想法是将整个char** 转为int**，之后使用intersect_Row_int进行处理
 */
 void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[MAXROW], int jj[MAXROW], int *cnt1, int *cnt2)
 {
@@ -62,12 +62,12 @@ void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[M
     {
         for(int j = 0 ; j < MAXCOL ; j ++ )
         {
-            if(a[i][j] != (char)0 )
+            if(a[i][j] != 0 )
             {
                 A[i][j] = a[i][j] ;
             }
             else A[i][j] = -1 ;
-            if(b[i][j] != (char)0)
+            if(b[i][j] != 0)
             {
                 B[i][j] = b[i][j] ;
             }
@@ -84,7 +84,7 @@ void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[M
         mp[i] = 0 ;
     }
     for(int i = 0; i < MAXROW; i++){
-        if(a[i][0] < 'A' || a[i][0] > 'z') break ;
+        if(a[i][0] < '0' || a[i][0] > '9') break ;
         long long res1 = 0;
         long long res2 = 0;
         for(int j = 0; j < MAXCOL; j++){
@@ -97,7 +97,7 @@ void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[M
 
     // int cnt1 = 0, cnt2 = 0;
     for(int i = 0; i < MAXROW; i++){
-        if(b[i][0] < 'A' || b[i][0] > 'z') break ;
+        if(b[i][0] < '0' || b[i][0] > '9') break ;
         long long res1 = 0;
         long long res2 = 0;
         for(int j = 0; j < MAXCOL; j++){
@@ -124,6 +124,17 @@ void intersect_Row_char(char a[MAXROW][MAXCOL], char b[MAXROW][MAXCOL], int ii[M
 
 int main()
 {
-    
+    char a[MAXROW][MAXCOL], b[MAXROW][MAXCOL] ;
+    strcpy(a[0], "1314520") ;
+    strcpy(a[1], "1314210") ;
+    strcpy(b[0], "1314210") ;
+    int ii[MAXROW] ;
+    int jj[MAXROW] ;
+    int cnt1, cnt2 ;
+    intersect_Row_char(a, b, ii, jj, &cnt1, &cnt2) ;
+    for(int i = 1; i <= cnt1 ; i ++ )
+    {
+        printf("%d\n", ii[i]) ;
+    }
     return 0 ;
 }
